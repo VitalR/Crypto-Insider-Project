b2e958a9d03b48fe6f7196b3c13ff1615d15b5e0
VitalR <vitalii.rodikov@gmail.com>
2020-08-24 08:33:41 +0300

    Add first step of Crypto Api functionality

--- cryptoinsider_project/cryptonews/views.py
+++ cryptoinsider_project/cryptonews/views.py
@@ -1,5 +1,10 @@
+import json
+import requests
+
 from django.shortcuts import render
 
 
 def home(request):
-    return render(request, 'cryptonews/home.html')
+    news_api_request = requests.get('https://min-api.cryptocompare.com/data/v2/news/?lang=EN')
+    news_api = json.loads(news_api_request.content)
+    return render(request, 'cryptonews/home.html', {'news_api': news_api})
