3678268fec29ca754a45daa8f821b76245c88d36
VitalR <vitalii.rodikov@gmail.com>
2020-08-27 23:58:46 +0300

    Add News, Prices and Search Coin views and templates

--- cryptoinsider_project/cryptonews/urls.py
+++ cryptoinsider_project/cryptonews/urls.py
@@ -4,4 +4,8 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),
+    path('search/', views.search_coin, name='search_coin'),
+    path('news/', views.news, name='news'),
+    path('prices/', views.prices, name='prices'),
+
 ]
--- cryptoinsider_project/cryptonews/views.py
+++ cryptoinsider_project/cryptonews/views.py
@@ -8,8 +8,32 @@ def home(request):
     price_request = requests.get('https://min-api.cryptocompare.com/data/pricemultifull?fsyms=BTC,ETH,XRP&tsyms=USD')
     price = json.loads(price_request.content)
 
-
     news_request = requests.get('https://min-api.cryptocompare.com/data/v2/news/?lang=EN')
     news = json.loads(news_request.content)
 
     return render(request, 'cryptonews/home.html', {'news': news, 'price': price})
+
+
+
+def search_coin(request):
+    if request.method == 'POST':
+        coin = request.POST['coin']
+        price_request = requests.get(
+            'https://min-api.cryptocompare.com/data/pricemultifull?fsyms='+coin+'&tsyms=USD')
+        price = json.loads(price_request.content)
+        return render(request, 'cryptonews/search_coin.html', {'coin': coin, 'price': price})
+    else:
+        return render(request, 'cryptonews/search_coin.html')
+
+
+def news(request):
+    news_request = requests.get('https://min-api.cryptocompare.com/data/v2/news/?lang=EN')
+    news = json.loads(news_request.content)
+    return render(request, 'cryptonews/news.html', {'news': news})
+
+
+def prices(request):
+    price_request = requests.get('https://min-api.cryptocompare.com/data/pricemultifull?fsyms=BTC,ETH,XRP,USDT,LINK,DOT,BCH,LTC,BSV,BNB,ADA,EOS,XTZ,XLM,TRX,XMR,USDC,NEO,VET,MIOTA,DASH,ETC,ZEC,LEND,ICX,WAVES,MKR,YFI&tsyms=USD')
+    price = json.loads(price_request.content)
+
+    return render(request, 'cryptonews/home.html', {'price': price})
