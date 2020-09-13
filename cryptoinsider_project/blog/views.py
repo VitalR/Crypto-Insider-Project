d3659984f8506854e644d93a1547056e015f683b
VitalR <vitalii.rodikov@gmail.com>
2020-09-13 08:56:04 +0300

    Added Blog Post Model

--- cryptoinsider_project/blog/views.py
+++ cryptoinsider_project/blog/views.py
@@ -1,25 +1,10 @@
 from django.shortcuts import render, redirect
-
-posts = [
-    {
-        'author': 'John Snow',
-        'title': 'Blog Post',
-        'content': 'This is post content',
-        'date_posted': 'September 12, 2020'
-    },
-    {
-        'author': 'Test User',
-        'title': 'Blog Post 2',
-        'content': 'This is post content 2',
-        'date_posted': 'September 12, 2020'
-    }
-
-]
+from .models import Post
 
 
 def blog(request):
     context = {
-        'posts': posts
+        'posts': Post.objects.all()
     }
     return render(request, 'blog/blog.html', context)
 
