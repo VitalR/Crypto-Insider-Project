4020285164d963398ca5d9727e77150c520b24cc
VitalR <vitalii.rodikov@gmail.com>
2020-09-16 11:32:34 +0300

    Create, Update and Delete Posts func

--- cryptoinsider_project/blog/views.py
+++ cryptoinsider_project/blog/views.py
@@ -1,4 +1,10 @@
 from django.shortcuts import render, redirect
+from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
+from django.views.generic import (
+    ListView, DetailView,
+    CreateView, UpdateView,
+    DeleteView
+)
 from .models import Post
 
 
@@ -7,6 +13,55 @@ def blog(request):
         'posts': Post.objects.all()
     }
     return render(request, 'blog/blog.html', context)
+
+
+# Class based views
+class PostListView(ListView):
+    model = Post
+    template_name = 'blog/blog.html'  # <app>/<model>_<viewtype>.html
+    context_object_name = 'posts'
+    ordering = ['-date_posted']
+
+
+class PostDetailView(DetailView):
+    model = Post
+
+
+class PostCreateView(LoginRequiredMixin, CreateView):
+    model = Post
+    fields = ['title', 'content']
+
+    # Add author to the post - current logged user can create a post
+    def form_valid(self, form):
+        form.instance.author = self.request.user
+        return super().form_valid(form)
+
+
+class PostUpdateView(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
+    model = Post
+    fields = ['title', 'content']
+
+    def form_valid(self, form):
+        form.instance.author = self.request.user
+        return super().form_valid(form)
+
+    # UserPassesTestMixin: only author is able to update the post
+    def test_func(self):
+        post = self.get_object()
+        if self.request.user == post.author:
+            return True
+        return False
+
+
+class PostDeleteView(LoginRequiredMixin, UserPassesTestMixin, DeleteView):
+    model = Post
+    success_url = '/blog/'
+
+    def test_func(self):
+        post = self.get_object()
+        if self.request.user == post.author:
+            return True
+        return False
 
 
 def about(request):
